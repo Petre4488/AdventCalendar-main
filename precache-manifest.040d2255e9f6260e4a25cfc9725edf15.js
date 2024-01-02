ae20c1a38a93844418eb74cdff2afd9a947cb00e
Peter <petrecirceagwork@gmail.com>
2024-01-02 21:50:02 +0200

    Updates

---
+++ precache-manifest.040d2255e9f6260e4a25cfc9725edf15.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f230dae85ab856dd841152f946409c3",
+    "url": "/AdventCalendar/index.html"
+  },
+  {
+    "revision": "c477c3f3efd8e2900b04",
+    "url": "/AdventCalendar/static/js/2.59d78f4f.chunk.js"
+  },
+  {
+    "revision": "37eec512efff5d9f8180",
+    "url": "/AdventCalendar/static/js/main.4d1f5c95.chunk.js"
+  },
+  {
+    "revision": "b7ca6fefc3f464356623",
+    "url": "/AdventCalendar/static/js/runtime~main.feed173c.js"
+  },
+  {
+    "revision": "db2119c1056330bc88a66e0785b9a7ff",
+    "url": "/AdventCalendar/static/media/1.db2119c1.jpg"
+  },
+  {
+    "revision": "55c131d58f91b590c5785736622ad49d",
+    "url": "/AdventCalendar/static/media/10.55c131d5.jpg"
+  },
+  {
+    "revision": "55c131d58f91b590c5785736622ad49d",
+    "url": "/AdventCalendar/static/media/2.55c131d5.jpg"
+  },
+  {
+    "revision": "9a52c90ba856cb9bcf51b98930342ebb",
+    "url": "/AdventCalendar/static/media/3.9a52c90b.jpg"
+  },
+  {
+    "revision": "9a52c90ba856cb9bcf51b98930342ebb",
+    "url": "/AdventCalendar/static/media/4.9a52c90b.jpg"
+  },
+  {
+    "revision": "db2119c1056330bc88a66e0785b9a7ff",
+    "url": "/AdventCalendar/static/media/5.db2119c1.jpg"
+  },
+  {
+    "revision": "55c131d58f91b590c5785736622ad49d",
+    "url": "/AdventCalendar/static/media/6.55c131d5.jpg"
+  },
+  {
+    "revision": "db2119c1056330bc88a66e0785b9a7ff",
+    "url": "/AdventCalendar/static/media/7.db2119c1.jpg"
+  },
+  {
+    "revision": "55c131d58f91b590c5785736622ad49d",
+    "url": "/AdventCalendar/static/media/8.55c131d5.jpg"
+  },
+  {
+    "revision": "bc1b3c3b2838dd16205f52d87489e131",
+    "url": "/AdventCalendar/static/media/9.bc1b3c3b.jpg"
+  },
+  {
+    "revision": "b7975bb9c541a6490b6ddf608b6a3266",
+    "url": "/AdventCalendar/static/media/calendar_backdrop.b7975bb9.jpg"
+  },
+  {
+    "revision": "2443bb2f31d6633e56398ca8a7fddd17",
+    "url": "/AdventCalendar/static/media/door_backdrop.2443bb2f.jpg"
+  }
+]);
